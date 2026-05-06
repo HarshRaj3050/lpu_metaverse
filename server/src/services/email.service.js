e67c85cbc30addf3cc71e5a4b7a916ba03ee547f
HarshRaj3050 <harshraj3060@gmail.com>
2026-05-06 14:49:54 +0530

    update email service logic

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -1,20 +1,20 @@
 const nodemailer = require('nodemailer');
 const config = require('../config/config');
-const { generateOtp } = require('../utils/otp.utils');
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
-
 
 const sendEmail = async (to, subject, text, html) => {
   try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: config.EMAIL_USER,
+        pass: config.EMAIL_PASS
+      }
+    });
+
     const info = await transporter.sendMail({
-      from: `"Your Name" <${config.GOOGLE_USER}>`, // sender address
+      from: `"LPU Metaverse" <${config.EMAIL_USER}>`, // sender address
       to, // list of receivers
       subject, // Subject line
       text, // plain text body
@@ -22,11 +22,11 @@ const sendEmail = async (to, subject, text, html) => {
     });
 
     console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    return true;
   } catch (error) {
     console.error('Error sending email:', error);
+    throw error;
   }
 };
-
 
 module.exports = sendEmail;
